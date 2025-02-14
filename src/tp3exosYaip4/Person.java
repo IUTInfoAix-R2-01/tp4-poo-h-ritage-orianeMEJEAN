03f43ccd7aa35c81676924e302525a5d6301332b
oriane <oriane@oriane-VirtualBox>
2025-02-14 12:13:37 +0100

    Modification de Person

--- src/tp3exosYaip4/Person.java
+++ src/tp3exosYaip4/Person.java
@@ -26,7 +26,6 @@ public class Person
         this.adress = ad;
     }
     
-    @Override
     public String toString() 
     {
         return "Person [name = " + name + ", adress = " + adress + "]";
