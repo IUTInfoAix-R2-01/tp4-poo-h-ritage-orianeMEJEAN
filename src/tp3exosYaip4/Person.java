41c591272f091d259c84a7991b8d6462157bc13f
oriane <oriane@oriane-VirtualBox>
2025-02-14 12:02:28 +0100

    Modification de la class Person

--- src/tp3exosYaip4/Person.java
+++ src/tp3exosYaip4/Person.java
@@ -2,45 +2,46 @@ package tp3exosYaip4;
 
 public class Person 
 {
-	private static String name;
-	private static String adress;
-	
-	public Person(String n, String ad)
-	{
-		Person.name = n;
-		Person.adress = ad;
-	}
-	
-	public static String getName()
-	{
-		return name;
-	}
-	
-	public static String getAdress()
-	{
-		return adress;
-	}
-	
-	public void setAdress(String ad)
-	{
-		Person.adress = ad;
-	}
-	
-	public String toString()
-	{
-		return "Person [name = " + name + ", adress = " + adress + "]";
-	}
-	
-	//Main de Person
-	public static void main(String[] args)
-	{
-		Person p1 = new Person("Ichi", "123 Rue de Belgique"); //fausse adresse
+    private String name;
+    private String adress;
+    
+    public Person(String n, String ad) 
+    {  
+        this.name = n;
+        this.adress = ad;
+    }
+    
+    public String getName() 
+    {
+        return name;
+    }
+    
+    public String getAdress() 
+    {
+        return adress;
+    }
+    
+    public void setAdress(String ad) 
+    {
+        this.adress = ad;
+    }
+    
+    @Override
+    public String toString() 
+    {
+        return "Person [name = " + name + ", adress = " + adress + "]";
+    }
+    
+    // Main de Person
+    public static void main(String[] args) 
+    {
+        Person p1 = new Person("Ichi", "123 Rue de Belgique");  //fausse adress
         System.out.println(p1);
         
-        p1.setAdress("456 Rue de Belgique"); //fausse adresse
+        p1.setAdress("456 Rue de Belgique");  //fausse adress
         System.out.println(p1);
-        
-        System.out.println(Person.getName());
-        System.out.println(Person.getAdress());
-	}
+
+        System.out.println(p1.getName());
+        System.out.println(p1.getAdress());
+    }
 }
