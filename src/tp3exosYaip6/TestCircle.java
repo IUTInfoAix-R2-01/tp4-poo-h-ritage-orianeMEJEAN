a042e88be755f3ced8c6478ff81b637157be5b39
oriane <oriane@oriane-VirtualBox>
2025-03-01 09:42:33 +0100

    TestCircle de la Classe Circle de l'exo YAIP 6

---
+++ src/tp3exosYaip6/TestCircle.java
@@ -0,0 +1,12 @@
+package tp3exosYaip6;
+
+public class TestCircle
+{
+	public static void main(String[] args) 
+	{
+		Circle c1 = new Circle(4);
+	    System.out.println(c1);
+	    System.out.println(c1.getArea());
+	    System.out.println(c1.getPerimeter());
+	}
+}
