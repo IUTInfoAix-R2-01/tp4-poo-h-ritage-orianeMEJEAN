8ee619066cfade98aed9d3e96aca1b2d64717a53
oriane <oriane@oriane-VirtualBox>
2025-03-01 09:41:59 +0100

    Classe ResizableCircle de l'exo YAIP 6

---
+++ src/tp3exosYaip6/ResizableCircle.java
@@ -0,0 +1,21 @@
+package tp3exosYaip6;
+
+public class ResizableCircle extends Circle implements Resizable
+{	
+	public ResizableCircle(double radius)
+	{
+		super(radius);
+	}
+	
+	@Override
+	public String toString()
+	{
+		return "ResizableCircle = [" + radius + "]";
+	}
+	
+	@Override
+	public void resize(int percent)
+	{
+		radius *= (percent/100.0);
+	}
+}
